0a261c4e5751352c3c45b380a1a240b84042bb07
Trent Scholl <tscholl@console.com.au>
2016-05-17 16:25:08 +1000

    Adding PowerShell build script for scollector

---
+++ Build-scollector.ps1
@@ -0,0 +1,43 @@
+$env:GOOS = "windows"
+$env:GOARCH = "amd64"
+
+$OperatingSystems = @("windows","linux")
+$Architectures = @("amd64", "386")
+
+$VersionDate = Get-Date -Format yyyyMMddHHmmss
+$GitSHA = (git rev-parse HEAD) | Out-String
+
+
+Function Build
+{
+    Param([string]$os, [string]$arch)
+
+    $env:GOOS = $os
+    $env:GOARCH = $arch
+
+    $ext = ""
+
+    If ($os -eq "windows")
+    {
+        $ext = ".exe"
+    }
+
+    Write-Host "Building scollector for $os ($arch)"
+
+    go build -o "scollector-$os-$arch$ext" -ldflags "-X bosun.org/_version.VersionSHA=$GitSHA -X bosun.org/_version.OfficialBuild=true -X bosun.org/_version.VersionDate=$VersionDate" .\cmd\scollector 
+}
+
+Write-Host "Running collector tests..."
+
+& go test -v .\cmd\scollector\collectors\
+
+If ($LASTEXITCODE -ne 0)
+{
+    Exit $LASTEXITCODE
+}
+
+ForEach ($OperatingSystem in $OperatingSystems) {
+    ForEach ($Architecture in $Architectures) {
+        Build -os $OperatingSystem -arch $Architecture
+    }   
+}
